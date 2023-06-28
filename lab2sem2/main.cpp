00b36d40db46642c515531769419bc1708331274
vovilon228 <113108239+vovilon228@users.noreply.github.com>
2023-06-28 12:51:38 +0300

    Update main.cpp

--- lab2sem2/main.cpp
+++ lab2sem2/main.cpp
@@ -6,7 +6,7 @@
 #include <QVBoxLayout>
 #include <QMessageBox>
 
-// PrintData реалізує інтерфейс Strategy і надає алгоритм для друку даних.
+
 class PrintData : public Strategy {
 public:
     // Виконує алгоритм друку даних.
@@ -15,7 +15,7 @@ public:
     }
 };
 
-// SaveData реалізує інтерфейс Strategy і надає алгоритм для зберігання даних.
+
 class SaveData : public Strategy {
 public:
     // Виконує алгоритм зберігання даних.
@@ -24,10 +24,9 @@ public:
     }
 };
 
-// UpdateObserver реалізує інтерфейс Observer для реагування на оновлення від Subject.
 class UpdateObserver : public Observer {
 public:
-    // Реагує на оновлення, відображаючи сповіщення.
+
     void update() override {
         qDebug() << "Operation performed.";
         QMessageBox::information(nullptr, "Notification", "Operation performed successfully!");
@@ -42,34 +41,32 @@ int main(int argc, char *argv[]) {
     QPushButton button("Execute");
     QVBoxLayout layout;
 
-    // Контекст використовує об'єкти Strategy для виконання алгоритму.
     Context context(std::make_unique<PrintData>());
-    // Subject керує спостерігачами.
+
     Subject subject;
-    // Спостерігач реагує на оновлення від предмета.
+
     UpdateObserver observer;
 
-    // Налаштовуємо розташування вікна
+
     layout.addWidget(&comboBox);
     layout.addWidget(&button);
     window.setLayout(&layout);
     window.show();
 
-    // Додаємо елементи до комбінаторного поля з відповідною стратегією
+
     comboBox.addItem("Print", QVariant::fromValue(std::make_unique<PrintData>()));
     comboBox.addItem("Save", QVariant::fromValue(std::make_unique<SaveData>()));
 
-    // Підключаємо сигнал натискання кнопки "Виконати" до лямбди, яка виконує обрану стратегію.
     QObject::connect(&button, &QPushButton::clicked, [&]() {
-        // Встановлюємо стратегію залежно від вибору в комбінаторному полі.
+
         context.setStrategy(comboBox.currentData().value<std::unique_ptr<Strategy>>());
-        // Виконуємо обрану стратегію.
+
         context.executeStrategy();
-        // Приєднуємо спостерігача до предмета.
+
         subject.attach(&observer);
-        // Повідомляємо спостерігачів, що предмет оновлено.
+
         subject.notify();
-        // Від'єднуємо спостерігача після сповіщення.
+
         subject.detach(&observer);
     });
 
